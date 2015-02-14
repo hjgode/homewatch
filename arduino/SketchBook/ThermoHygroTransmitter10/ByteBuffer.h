b015b7c44c992d2c0b7935d6ab4ce4bed18d422d
hjgode <hjgode@gmail.com>
2015-02-14 18:58:18 +0100

    changed code of ThermoHygroTransmitter10 to send Outlet control command for ON or OFF on every cycle. I have seen occations where the outlet was switched ON but not by the ThermoHygroTransmitter code!

---
+++ arduino/SketchBook/ThermoHygroTransmitter10/ByteBuffer.h
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+
+#ifndef BYTEBUFFER_H
+#define BYTEBUFFER_H
+
+#define byte unsigned char
+
+class ByteBuffer
+{
+public:
+	ByteBuffer();
+
+	// This method initializes the datastore of the buffer to a certain sizem the buffer should NOT be used before this call is made
+	void init(unsigned int buf_size);
+
+	// This method resets the buffer into an original state (with no data)
+	void clear();
+
+	// This releases resources for this buffer, after this has been called the buffer should NOT be used
+	void deAllocate();
+
+	// Returns how much space is left in the buffer for more data
+	int getSize();
+
+	// Returns the maximum capacity of the buffer
+	int getCapacity();
+
+	// This method returns the byte that is located at index in the buffer but doesn't modify the buffer like the get methods (doesn't remove the retured byte from the buffer)
+	byte peek(unsigned int index);
+
+	//
+	// Put methods, either a regular put in back or put in front
+	//
+	int putInFront(byte in);
+	int put(byte in);
+
+	int push(byte in);	//HGO
+	byte getMedian();	//HGO
+	byte getMax();	//HGO
+	byte getMin();	//HGO
+	byte getState();	//HGO
+	void setTreshold(byte t);	//HGO
+	byte getTreshold();	//HGO
+    int getDirection();
+    int getStateDuration();
+
+	int putIntInFront(int in);
+	int putInt(int in);
+
+	int putLongInFront(long in);
+	int putLong(long in);
+
+	int putFloatInFront(float in);
+	int putFloat(float in);
+
+	//
+	// Get methods, either a regular get from front or from back
+	//
+	byte get();
+	byte getFromBack();
+
+	int getInt();
+	int getIntFromBack();
+
+	long getLong();
+	long getLongFromBack();
+
+	float getFloat();
+	float getFloatFromBack();
+
+    void setMaxDuration(int);
+
+protected:
+	byte* data;
+
+	unsigned int capacity;
+	unsigned int position;
+	unsigned int length;
+	byte state;
+	byte treshold;
+
+    int stateDuration;  //start if state=1 for
+    int maxDuration;    //max duration (ie 5)
+    int direction;
+};
+
+#endif // BYTEBUFFER_H
---
+++ arduino/SketchBook/ThermoHygroTransmitter10/ThermoHygroTransmitter9.ino
@@ -0,0 +1,237 @@
+//added LCD
+//adding intelligence...
+//added newBuffer
+//added RCswitch
+
+#include <RCSwitch.h>
+RCSwitch mySwitch;// = RCSwitch();
+#define RC_SWITCH_SYSTEM_CODE "00001"
+#define RC_SWITCH_SWITCH_CODE "00001"
+
+#include <Sleep_n0m1.h>
+Sleep sleep;
+unsigned long sleepTime; //how long you want the arduino to sleep
+
+#include <DHT.h>
+#define DHT_DATA_PIN  2
+static DHT sensor;
+static boolean bValidData=false;
+
+#define lcd_rs  8
+#define lcd_enable  9
+#define lcd_backlight 10
+// include the library code:
+#include <LiquidCrystal.h>
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(lcd_rs, lcd_enable, 4, 5, 6, 7);
+char* lcdAnim[] = {"|","/","-","\\"};
+
+#include "newBuffer.h"
+newBuffer send_buffer;
+unsigned char state;
+
+static byte sendCount=1;
+const byte sendInterval=6; //send every x wakeups
+
+int lastTemp=0;
+int lastHumi=0;
+
+#define SERIAL_BAUD 9600
+
+#define led 13
+
+ #include <SensorTransmitter.h>
+ 
+ #define TX_DATA_PIN  3
+ #define TX_RAND_ID  4
+ #define TX_CHANNEL  4
+ 
+ // Initializes a ThermoHygroTransmitter on pin 11, with "random" ID 0, on channel 1.
+ ThermoHygroTransmitter transmitter(TX_DATA_PIN, TX_RAND_ID, TX_CHANNEL);
+ 
+ void blinkLED(){
+   digitalWrite(led, HIGH);
+   delay(500);
+   digitalWrite(led, LOW);
+ }
+ 
+ void blinkLEDfast(){
+   digitalWrite(led, HIGH);
+   delay(200);
+   digitalWrite(led, LOW);
+   delay(200);
+   digitalWrite(led, HIGH);
+   delay(200);
+   digitalWrite(led, LOW);
+   delay(200);
+   digitalWrite(led, HIGH);
+   delay(200);
+   digitalWrite(led, LOW);
+   delay(200);
+ }
+
+void showLCD(int temp, int humi, unsigned char state){
+  lcd.setCursor(0, 1); //setCursor(col,row)
+  lcd.print(temp);
+  lcd.print("*C ");
+  lcd.print(humi);
+  lcd.print("%");
+  lcd.print("  ");
+  if(state==0)
+    lcd.print("off");
+  else
+    lcd.print("ON ");
+    
+  //read long time Min value
+  byte bMin = send_buffer.getMin2();
+  
+  lcd.print(" ");
+  lcd.print(bMin);
+  
+  lcdON();
+//  delay(3000);
+//  lcdOFF();
+}
+
+void lcdON(){
+  digitalWrite(lcd_backlight, HIGH);
+}
+void lcdOFF(){
+  digitalWrite(lcd_backlight, LOW);
+}
+
+//switch ELRO wireless power outlet
+void switchOnOff(bool bOnOff){
+  if(bOnOff){
+    Serial.println("SWITCH ON");
+    mySwitch.switchOn(RC_SWITCH_SYSTEM_CODE, RC_SWITCH_SWITCH_CODE);
+  }
+  else{
+    Serial.println("SWITCH OFF");
+    mySwitch.switchOff(RC_SWITCH_SYSTEM_CODE, RC_SWITCH_SWITCH_CODE);
+  }
+}
+
+void animateLCD(){
+  static int iCnt=0;
+  lcd.setCursor(14,0);
+  lcd.print(lcdAnim[iCnt]);
+  iCnt++;
+  if(iCnt==4)
+    iCnt=0;
+}
+
+void setup(){
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("ThermoHygroTransmitter version 4 (power save 2)");
+  sensor.setup(DHT_DATA_PIN);
+    
+  sleepTime=10000; //10 seconds 
+  
+  pinMode(lcd_backlight, OUTPUT);
+
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Temp/Feuchte"); //12 chars
+//  lcdOFF();  
+  lcdON();
+  // Initialize the send buffer that we will use to send data
+  //ByteBuffer send_buffer;
+  send_buffer.init(5);
+  send_buffer.setTreshold(5);
+  send_buffer.setMaxDuration1(10);
+
+  //init RCswitch instance
+  mySwitch = RCSwitch();
+  // Transmitter is connected to Arduino Pin #3  
+  // shared with SensorTransmitter code 
+  mySwitch.enableTransmit(TX_DATA_PIN);
+  // Optional set protocol (default is 1, will work for most outlets)
+  mySwitch.setProtocol(1);
+  
+  // Optional set number of transmission repetitions.
+  //mySwitch.setRepeatTransmit(2); //stops working!
+  
+  //initialy switch power outlet OFF
+  switchOnOff(false);
+}
+
+void loop(){
+  //do stuff
+    animateLCD();
+    Serial.println("...waked up");
+    float humidity = sensor.getHumidity();
+    float temperature = sensor.getTemperature();
+/*
+  Serial.println(humidity, 1);
+  Serial.println(temperature, 1);
+  Serial.println(sensor.getStatusString());
+*/
+    if(strcmp(sensor.getStatusString(),"OK")==0){
+      bValidData=true;
+      lastTemp=(int)temperature;
+      lastHumi=(int)humidity;     
+      showLCD(lastTemp, lastHumi, send_buffer.getState1());
+    }
+    else{
+      bValidData=false;
+    }
+    Serial.print("sendCount="); Serial.println(sendCount);
+    if(sendCount >= sendInterval){
+      sendCount=0;
+     // Displays temperatures from -10 degrees Celsius to +20,
+     // and humidity from 10% REL to 40% REL, with increments of 2
+     if(bValidData) {
+
+        send_buffer.push((byte)lastHumi);
+        state=send_buffer.getState1();
+        
+        //on OFF switching moved outside loop
+        //to switch OFF if not state! and switched erratically by another sender
+        
+        int x=0;
+        Serial.println("============================");
+        for(x=0; x<send_buffer.getSize1(); x++){
+          Serial.print("["); 
+          Serial.print(send_buffer.peek1(x));
+          Serial.print("]"); 
+        }
+        Serial.println("\n============================");
+
+       // Temperatures are passed at 10 times the real value,
+       // to avoid using floating point math.
+       ///transmitter.sendTempHumi(i * 10, i + 20);
+       //HACK: send temp with ending in 5 (which is .5 degree clesius if state==1
+       // if state==0 send temp with ending in 0
+       if(state==1)
+         transmitter.sendTempHumi(lastTemp * 10 + 5, lastHumi);
+       else
+         transmitter.sendTempHumi(lastTemp * 10, lastHumi);
+       
+       Serial.println("Transmit done");
+       blinkLEDfast();
+       // Wait two seconds before sending next.
+       bValidData=false;
+       //showLCD(lastTemp, lastHumi, state); //movded ouside loop
+       
+     }//validData
+   }//send_loop
+   
+   showLCD(lastTemp, lastHumi, state);
+
+   //send switch command
+   if(state==0)
+     switchOnOff(false);
+   else
+     switchOnOff(true);
+
+   //delay(1000);
+  sendCount++;
+  blinkLED();
+
+  Serial.println("About to sleep...");
+  delay(200);
+  sleep.pwrDownMode(); //set sleep mode
+  sleep.sleepDelay(sleepTime); //sleep for: sleepTime
+}
+
